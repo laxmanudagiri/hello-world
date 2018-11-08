45d3fdf1e54de9813d45a3be0c218f6f220fb80d
laxmanudagiri <laxman.udagiri@gmail.com>
2018-11-08 22:08:40 +0530

    Test Del
    Test del

--- Python/InLecturePythonForLoopExample.py
+++
@@ -1,3 +0,0 @@
-device = ['r1', 'r2', 'r3']
-for blah in device:
-     print "blah can be any word.  Device is {}".format(blah)
--- Python/pnp_config.py
+++
@@ -1,3 +0,0 @@
-CONFIGS_DIR= "work_files/configs/"
-DEVICES="work_files/inventory.csv"
-TEMPLATE="work_files/templates/config_template.jnj"
